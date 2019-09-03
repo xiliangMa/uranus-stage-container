c58200e7bca63a7a43969bc81adba3bb6a282788
soaop <fengjin@uranus.io>
2019-09-03 18:06:10 +0800

    HOST_IP auto detect, dc mananger script

--- proxy_dev_server/proxy-rancher.entrypoint.sh
+++ proxy_dev_server/proxy-rancher.entrypoint.sh
@@ -2,13 +2,12 @@
 
 # sed -i 's/----->HOST_IP<-----/'$HOST_IP/ /nodejs-proxy/dist/serverConfig.js
 
-## use for apt install and modify in container
+##------ use for apt install and modify in container
 # apt-get install apt-transport-https
-apt-get update
-apt-get install vim -y
-alias ll='ls -l'
+# apt-get update
+# apt-get install vim -y
+# alias ll='ls -l'
 
 node bin/index
-
 
 exec "$@"
--- uranus-stage-ui/nginx-ui-dist.entrypoint.sh
+++ uranus-stage-ui/nginx-ui-dist.entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sed -i 's/----->HOST_IP<-----/'$HOST_IP/ /opt/dist/static/serverConfig.js
-sed -i 's/----->HOST_IP<-----/'$HOST_IP/ /etc/nginx/conf.d/ui-rancher.proxy.conf
+
+sed -i "s/----->HOST_IP<-----/$1/" /opt/dist/static/serverConfig.js
+sed -i "s/----->HOST_IP<-----/$1/" /etc/nginx/conf.d/ui-rancher.proxy.conf
+
 
 cat /opt/dist/static/serverConfig.js
 cat /etc/nginx/conf.d/ui-rancher.proxy.conf
