2adacb6d9fed87e8e5da067e3030c66aa4d608d8
soaop <fengjin@uranus.io>
2019-09-04 11:39:15 +0800

    add index.html (with /) to rancher proxy server(on 8088)

--- proxy_dev_server/proxy-rancher.entrypoint.sh
+++ proxy_dev_server/proxy-rancher.entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# sed -i 's/----->HOST_IP<-----/'$HOST_IP/ /nodejs-proxy/dist/serverConfig.js
-
 ##------ use for apt install and modify in container
 # apt-get install apt-transport-https
 # apt-get update
