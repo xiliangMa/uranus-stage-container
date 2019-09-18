69572474fdee989db5ca9796f3be39b641411522
soaop <soaop@sina.cn>
2019-09-18 03:26:36 +0000

    Build dist config and build_dir cache volume , access.log to volume

--- uranus-stage-ui/build-dist-container.sh
+++ uranus-stage-ui/build-dist-container.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-cd /build_dir/uranus-stage
+echo  " rebuid build dist dir : ${BUILD_DIST_PARA}"
 
-npm install --registry=https://registry.npm.taobao.org  2>/dev/null
-npm run build:dll
-npm run build
-
+if [ "${BUILD_DIST_PARA}" == "Y" ]; then
+    cd /build_dir/uranus-stage
+    
+    npm install --registry=https://registry.npm.taobao.org  2>/dev/null
+    npm run build:dll
+    npm run build
+else
+    echo 'use cached dist dir content'
+    mkdir /build_dir/uranus-stage/dist
+fi
 
--- uranus-stage-ui/nginx-ui-dist.entrypoint.sh
+++ uranus-stage-ui/nginx-ui-dist.entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 
-sed -i "s/----->HOST_IP<-----/$1/" /opt/dist/static/serverConfig.js
-sed -i "s/----->JAVASRV_IP<-----/$2/" /opt/dist/static/serverConfig.js
-sed -i "s/----->HOST_IP<-----/$1/" /etc/nginx/conf.d/ui-rancher.proxy.conf
-sed -i "s/----->JAVASRV_IP<-----/$2/" /etc/nginx/conf.d/java-api.proxy.conf
+sed -i "s/----->HOST_IP<-----/${HOST_IP_ENT}/" /opt/dist/static/serverConfig.js
+sed -i "s/----->JAVASRV_IP<-----/${JAVASRV_IP_ENT}/" /opt/dist/static/serverConfig.js
+sed -i "s/----->HOST_IP<-----/${HOST_IP_ENT}/" /etc/nginx/conf.d/ui-rancher.proxy.conf
+sed -i "s/----->JAVASRV_IP<-----/${JAVASRV_IP_ENT}/" /etc/nginx/conf.d/java-api.proxy.conf
 
 
 cat /opt/dist/static/serverConfig.js
 cat /etc/nginx/conf.d/ui-rancher.proxy.conf | grep server_name
 cat /etc/nginx/conf.d/java-api.proxy.conf | grep proxy_pass
 
+if [ $BUILD_DIST_PARA = "Y" ];then
+     if [ ! -d "/build_dir/uranus-stage" ];then
+	mkdir /build_dir/uranus-stage
+     fi
+	cp -r /opt/dist /build_dir/uranus-stage/
+else
+     cp -r /build_dir/uranus-stage/dist/* /opt/dist	
+fi
+
+# ln -sf /dev/stdout /var/log/nginx/access.log
+# ln -sf /dev/stderr /var/log/nginx/error.log
+
 nginx -g "daemon off;"
 
-exec "$@"
+#exec "$@"
