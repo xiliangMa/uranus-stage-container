dd28039d4bee5820a9e333a81d9570b5a331ffd7
soaop <soaop@sina.cn>
2019-09-25 02:56:13 +0000

    fix ARG IMGSRV_PORT (img-server), split compose for ui-dev

--- uranus-stage-ui/nginx-ui-dist.entrypoint.sh
+++ uranus-stage-ui/nginx-ui-dist.entrypoint.sh
@@ -3,11 +3,11 @@
 
 sed -i "s/----->HOST_IP<-----/${HOST_IP_ENT}/" /opt/dist/static/serverConfig.js
 sed -i "s/----->JAVASRV_IP<-----/${JAVASRV_IP_ENT}/" /opt/dist/static/serverConfig.js
-sed -i "s/----->IMGSRV_PORT<-----/${IMGSRV_PORT}/" /opt/dist/static/serverConfig.js
+sed -i "s/----->IMGSRV_PORT<-----/${IMGSRV_PORT_ENT}/" /opt/dist/static/serverConfig.js
 sed -i "s/----->HOST_IP<-----/${HOST_IP_ENT}/" /etc/nginx/conf.d/ui-rancher.proxy.conf
 sed -i "s/----->HOST_IP<-----/${HOST_IP_ENT}/" /etc/nginx/conf.d/img-server.proxy.conf
 sed -i "s/----->JAVASRV_IP<-----/${JAVASRV_IP}/" /etc/nginx/conf.d/img-server.proxy.conf
-sed -i "s/----->IMGSRV_PORT<-----/${IMGSRV_PORT}/" /etc/nginx/conf.d/img-server.proxy.conf
+sed -i "s/----->IMGSRV_PORT<-----/${IMGSRV_PORT_ENT}/" /etc/nginx/conf.d/img-server.proxy.conf
 sed -i "s/----->JAVASRV_IP<-----/${JAVASRV_IP_ENT}/" /etc/nginx/conf.d/java-api.proxy.conf
 
 
@@ -15,14 +15,14 @@ cat /opt/dist/static/serverConfig.js
 cat /etc/nginx/conf.d/ui-rancher.proxy.conf | grep server_name
 cat /etc/nginx/conf.d/java-api.proxy.conf | grep proxy_pass
 
-if [ $BUILD_DIST_PARA = "Y" ];then
-    if [ ! -d "/build_dir/uranus-stage" ];then
-	    mkdir /build_dir/uranus-stage
-    fi
-	cp -r /opt/dist /build_dir/uranus-stage/
-else
-    # cp -r /build_dir/uranus-stage/dist/* /opt/dist
-fi
+# if [ $BUILD_DIST_PARA = "Y" ];then
+#    if [ ! -d "/build_dir/uranus-stage" ];then
+#	    mkdir /build_dir/uranus-stage
+#    fi
+#	cp -r /opt/dist /build_dir/uranus-stage/
+# else
+    cp -r /build_dir/uranus-stage/dist/* /opt/dist
+# fi
 
 nginx -g "daemon off;"
 
